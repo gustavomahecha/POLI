5bed0d62b39ce4fca0a977e71b0e4a0c1ef35ac2
gustavomahecha <66050283+gustavomahecha@users.noreply.github.com>
2025-09-08 21:22:44 -0500

    Delete config directory

--- config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:host=localhost;dbname=carmesy_db;charset=utf8', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die('Error de conexión: ' . $e->getMessage());
-}
-?>
